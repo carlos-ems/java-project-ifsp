5f7966d3e07589af4f926b7bff271e95487dab70
carlos <cmsilva1601@gmail.com>
2025-07-11 15:45:27 -0300

    Configuração da conexão com o banco de dados

--- src/ConexaoBancoDeDados.java
+++ src/ConexaoBancoDeDados.java
@@ -1,3 +1,11 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
 public class ConexaoBancoDeDados {
-    
+    private static final String urlDeConexao = "jdbc:sqlite:tarefas.db";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(urlDeConexao);
+    }
 }
