98fa5f00d26d285d5fe6d0790817a70b0e958045
carlos <cmsilva1601@gmail.com>
2025-07-11 23:41:02 -0300

    [Implementado por Mariana Keiko] Aplicação da lógica de negócio

--- src/TarefaService.java
+++ src/TarefaService.java
@@ -1,3 +1,26 @@
+import java.util.List;
+
 public class TarefaService {
-    
+    private final TarefaSQL consulta = new TarefaSQL();
+
+    public void adicionarTarefa(String nomeTarefa, String descricao) {
+        Tarefa tarefa = new Tarefa(nomeTarefa, descricao);
+        consulta.inserir(tarefa);
+    }
+
+    public List<Tarefa> listarTodas() {
+        return consulta.listarTodas();
+    }
+
+    public List<Tarefa> listarPendentes() {
+        return consulta.listarPorFiltro("PENDENTES");
+    }
+
+    public List<Tarefa> listarConcluidas() {
+        return consulta.listarPorFiltro("CONCLUIDAS");
+    }
+
+    public void alterarStatus(String nomeTarefa, boolean concluido) {
+        consulta.atualizarStatus(nomeTarefa, concluido);
+    }
 }
